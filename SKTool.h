2bcbb47825513ba4348cdffb820ba71be0acb823
trimble <trimble.zhang@outlook.com>
2018-11-27 10:11:38 +0800

    update

--- SKTool.h
+++ SKTool.h
@@ -7,13 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import <UIKit/UIKit.h>
 @interface SKTool : NSObject
+
+
+
+
+
+/**  获取当前的时间 */
+
+
++ (NSString *)getCurrentDateStrWithSepStr:(NSString *)sepStr;
+
++ (NSString *)getCurrentDateStrWithSepStr:(NSString *)sepStr haveMin:(BOOL)haveMin;
+
 
 /**  将时间戳字符串转换为时间 */
 + (NSString *)changeTimestampWithStr:(NSString *)stamp;
-
-
 
 /** 输入省份的名字 获取对应的省份编码code */
 + (NSString *)getProviceCodeWithString:(NSString *)provinceStr;
@@ -29,6 +39,51 @@
 
 /** 判断是否是银行卡号 */
 + (BOOL)isBankCard:(NSString *)cardNumber;
+
+/** 改变一个字符串指定起始结束位置的颜色 大小 */
++ (NSMutableAttributedString *)changeStringColorWithString:(NSString *)str Color:(UIColor *)color fontSize:(CGFloat)font startIndex:(NSInteger)startIdx endIndex:(NSInteger)endIdx;
+
+/** 改变一个textfieldholder的颜色以及大小 */
++ (UITextField *)changeTextfield:(UITextField *)textfield holderColor:(UIColor *)color holderFont:(CGFloat)fontSize;
+
+/** 是否包含中文 */
++(BOOL)isStringContainNumberAndLetterWith:(NSString *)str;
+
+/** MARK: 是否只包含数字 使用NSScaner*/
++(BOOL)isPureNumberString:(NSString *)string;
+
+
+/**单纯改变句子的字间距*/
++ (NSMutableAttributedString *)sk_changeStringSpaceWithTargetString:(NSString *)targetString Space:(CGFloat)space;
+
+/**  单纯改变段落的行间距 */
++ (NSMutableAttributedString *)sk_changeLineSpaceWithTargetString:(NSString *)targetString LineSpace:(CGFloat)lineSpace;
+
+/** 同时更改行间距和字间距*/
++ (NSMutableAttributedString *)sk_changeColAndRowSpaceWithTargetString:(NSString *)targetString rowSpace:(CGFloat)rowSpace colSpace:(CGFloat)colSpace;
+
+/**
+ *  改变某些文字的颜色 并单独设置其字体 改变最后一个
+ *
+ *  @param font        设置的字体
+ *  @param color       颜色
+ *  @param targetString 总的字符串
+ *  @param subArray    想要变色的字符数组
+ *
+ *  @return 生成的富文本
+ */
++ (NSMutableAttributedString *)sk_changeFontAndColor:(UIFont *)font Color:(UIColor *)color TargetString:(NSString *)targetString SubStringArray:(NSArray *)subArray;
+
+/**
+ *  单纯改变一句话中的某些字的颜色（一种颜色）改变所有
+ *
+ *  @param color    需要改变成的颜色
+ *  @param targetString 总的字符串
+ *  @param subArray 需要改变颜色的文字数组(要是有相同的 只取第一个)
+ *
+ *  @return 生成的富文本
+ */
++ (NSMutableAttributedString *)sk_changeCorlorWithColor:(UIColor *)color TargetString:(NSString *)targetString SubStringArray:(NSArray *)subArray;
 
 
 
--- SKTool.m
+++ SKTool.m
@@ -7,8 +7,61 @@
 //
 
 #import "SKTool.h"
-
+#import <CoreText/CoreText.h>
 @implementation SKTool
+
++ (NSString *)getCurrentDateStrWithSepStr:(NSString *)sepStr
+{
+
+	NSDate *currentDate = [NSDate date];//获取当前时间，日期
+	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+	if (sepStr == nil) {
+    	[dateFormatter setDateFormat:@"YYYY/MM/dd"];
+	}else if ([sepStr isEqualToString:@"."]){
+		[dateFormatter setDateFormat:@"YYYY.MM.dd"];
+	}else if ([sepStr isEqualToString:@"-"]){
+		[dateFormatter setDateFormat:@"YYYY-MM-dd"];
+	}
+	
+	NSString *dateString = [dateFormatter stringFromDate:currentDate];
+	return dateString;
+
+}
+
+
++ (NSString *)getCurrentDateStrWithSepStr:(NSString *)sepStr haveMin:(BOOL)haveMin
+{
+
+	NSDate *currentDate = [NSDate date];//获取当前时间，日期
+	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+	if (sepStr == nil) {
+		if (haveMin) {
+			[dateFormatter setDateFormat:@"YYYY/MM/dd hh:mm"];
+		}else{
+			[dateFormatter setDateFormat:@"YYYY/MM/dd"];
+		}
+	}else if ([sepStr isEqualToString:@"."]){
+		if (haveMin) {
+			[dateFormatter setDateFormat:@"YYYY.MM.dd hh:mm"];
+		}else{
+			[dateFormatter setDateFormat:@"YYYY.MM.dd"];
+		}
+		
+	}else if ([sepStr isEqualToString:@"-"]){
+		if (haveMin) {
+			[dateFormatter setDateFormat:@"YYYY-MM-dd hh:mm"];
+		}else{
+			[dateFormatter setDateFormat:@"YYYY-MM-dd"];
+		}
+		
+	}
+	
+	
+	
+	NSString *dateString = [dateFormatter stringFromDate:currentDate];
+	return dateString;
+
+}
 
 /** 时间戳转为时间 */
 + (NSString *)changeTimestampWithStr:(NSString *)stamp
@@ -210,5 +263,275 @@
 	return modulus == 0;
 
 }
+
+
+/**  MARK: 改变一个字符串指定起始结束位置的颜色 大小 */
++ (NSMutableAttributedString *)changeStringColorWithString:(NSString *)str Color:(UIColor *)color fontSize:(CGFloat )font startIndex:(NSInteger)startIdx endIndex:(NSInteger)endIdx{
+	
+	NSMutableAttributedString *noteStr = [[NSMutableAttributedString alloc] initWithString:str];
+    // 需要改变的第一个文字的位置
+    NSUInteger firstLoc = startIdx;
+    // 需要改变的最后一个文字的位置
+    NSUInteger secondLoc = endIdx;
+	
+	secondLoc = secondLoc > str.length ? str.length : secondLoc;
+    // 需要改变的区间
+    NSRange range = NSMakeRange(firstLoc, secondLoc - firstLoc);
+    // 改变颜色
+    [noteStr addAttribute:NSForegroundColorAttributeName value:color range:range];
+	[noteStr addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:font] range:range];
+	
+	return noteStr;
+
+}
+
+/** MARK: 改变textfield holer的字体 */
++(UITextField *)changeTextfield:(UITextField *)textfield holderColor:(UIColor *)color holderFont:(CGFloat)fontSize
+{
+	
+	[textfield setValue:color forKeyPath:@"_placeholderLabel.textColor"];
+	[textfield setValue:[UIFont boldSystemFontOfSize:fontSize] forKeyPath:@"_placeholderLabel.font"];
+	return textfield;
+}
+
+
+
+
+/** MARK:判断一个字符书不是中文。主要是将oc字符串转成c的字符串(char*)，然后判断c的字符串的长度，因为中文字符占得长度是英文字符的两倍。*/
++(BOOL)isContainChinese:(NSString*)string{
+
+    int strlength = 0;
+    char* p = (char*)[string cStringUsingEncoding:NSUnicodeStringEncoding];
+    for (int i=0 ; i<[string lengthOfBytesUsingEncoding:NSUnicodeStringEncoding] ;i++) {
+        if (*p) {
+
+            p++;
+
+            strlength++;
+
+        }
+
+        else {
+
+            p++;
+
+        }
+
+    }
+
+    return ((strlength/2)==1)?YES:NO;
+
+}
+
+/** MARK: 是否包含字母和数字 */
++(BOOL)isStringContainNumberAndLetterWith:(NSString *)str {
+
+	NSRegularExpression *numberRegular = [NSRegularExpression regularExpressionWithPattern:@"A-Za-z0-9]" options:NSRegularExpressionCaseInsensitive error:nil];
+
+	NSInteger count = [numberRegular numberOfMatchesInString:str options:NSMatchingReportProgress range:NSMakeRange(0, str.length)];
+
+//count是str中包含[A-Za-z0-9]数字的个数，只要count>0，说明str中包含数字
+
+	if (count > 0) {
+
+		return YES;
+
+	}
+
+	return NO;
+
+}
+
+/** MARK: 是否只包含数字 使用NSScaner*/
++(BOOL)isPureNumberString:(NSString *)string{
+	
+	NSScanner* scan = [NSScanner scannerWithString:string];
+	int val;
+	// - (BOOL)scanInt:(int *)value;  //是否找到一个十进制 int
+	return [scan scanInt:&val] && [scan isAtEnd];
+
+}
+
+#pragma mark - 改变字符串的字间距
+/**
+ *  单纯改变句子的字间距（需要 <CoreText/CoreText.h>）
+ *
+ *  @param targetString 需要更改的字符串
+ *  @param space       字间距
+ *
+ *  @return 生成的富文本
+ */
++ (NSMutableAttributedString *)sk_changeStringSpaceWithTargetString:(NSString *)targetString Space:(CGFloat)space{
+    
+    NSMutableAttributedString *attributedStr = [[NSMutableAttributedString alloc] initWithString:targetString];
+    long number = space;
+    CFNumberRef num = CFNumberCreate(kCFAllocatorDefault,kCFNumberSInt8Type,&number);
+    [attributedStr addAttribute:(id)kCTKernAttributeName value:(__bridge id)num range:NSMakeRange(0,[attributedStr length])];
+    CFRelease(num);
+    return attributedStr;
+    
+}
+#pragma mark - 改变字符串的行间距
+/**
+ *  单纯改变段落的行间距
+ *
+ *  @param targetString 需要更改的字符串
+ *  @param lineSpace   行间距
+ *
+ *  @return 生成的富文本
+ */
++ (NSMutableAttributedString *)sk_changeLineSpaceWithTargetString:(NSString *)targetString LineSpace:(CGFloat)lineSpace{
+    
+    
+    NSMutableAttributedString *attributedStr = [[NSMutableAttributedString alloc] initWithString:targetString];
+    NSMutableParagraphStyle * paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    [paragraphStyle setLineSpacing:lineSpace];
+    
+    [attributedStr addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, [targetString length])];
+    return attributedStr;
+    
+}
+
+#pragma mark - 改变字符串行间距和竖间距
+/**
+ *  同时更改行间距和字间距
+ *
+ *  @param targetString 需要改变的字符串
+ *  @param rowSpace   行间距
+ *  @param colSpace   字间距
+ *
+ *  @return 生成的富文本
+ */
++ (NSMutableAttributedString *)sk_changeColAndRowSpaceWithTargetString:(NSString *)targetString rowSpace:(CGFloat)rowSpace colSpace:(CGFloat)colSpace
+{
+    
+    NSMutableAttributedString *attributedStr = [[NSMutableAttributedString alloc] initWithString:targetString];
+    NSMutableParagraphStyle * paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    [paragraphStyle setLineSpacing:rowSpace];
+    [attributedStr addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, [targetString length])];
+    long number = colSpace;
+    CFNumberRef num = CFNumberCreate(kCFAllocatorDefault,kCFNumberSInt8Type,&number);
+    [attributedStr addAttribute:(id)kCTKernAttributeName value:(__bridge id)num range:NSMakeRange(0,[attributedStr length])];
+    CFRelease(num);
+    return attributedStr;
+    
+}
+
+#pragma mark - 改变某些文字的颜色 并单独设置其字体
+/**
+ *  改变某些文字的颜色 并单独设置其字体
+ *
+ *  @param font        设置的字体
+ *  @param color       颜色
+ *  @param targetString 总的字符串
+ *  @param subArray    想要变色的字符数组
+ *
+ *  @return 生成的富文本
+ */
++ (NSMutableAttributedString *)sk_changeFontAndColor:(UIFont *)font Color:(UIColor *)color TargetString:(NSString *)targetString SubStringArray:(NSArray *)subArray
+{
+    NSMutableAttributedString *attributedStr = [[NSMutableAttributedString alloc] initWithString:targetString];
+    
+    for (NSString *rangeStr in subArray) {
+        
+        NSRange range = [targetString rangeOfString:rangeStr options:NSBackwardsSearch];
+        
+        [attributedStr addAttribute:NSForegroundColorAttributeName value:color range:range];
+        [attributedStr addAttribute:NSFontAttributeName value:font range:range];
+    }
+    
+    return attributedStr;
+    
+}
+#pragma mark - 改变一句话中的某些字的颜色
+/**
+ *  单纯改变一句话中的某些字的颜色（一种颜色）改变所有
+ *
+ *  @param color    需要改变成的颜色
+ *  @param targetString 总的字符串
+ *  @param subArray 需要改变颜色的文字数组(要是有相同的 只取第一个)
+ *
+ *  @return 生成的富文本
+ */
++ (NSMutableAttributedString *)sk_changeCorlorWithColor:(UIColor *)color TargetString:(NSString *)targetString SubStringArray:(NSArray *)subArray{
+    
+    NSMutableAttributedString *attributedStr = [[NSMutableAttributedString alloc] initWithString:targetString];
+    for (NSString *rangeStr in subArray) {
+        
+        NSMutableArray *array = [self sk_getRangeWithTotalString:targetString SubString:rangeStr];
+        
+        for (NSNumber *rangeNum in array) {
+            
+            NSRange range = [rangeNum rangeValue];
+            [attributedStr addAttribute:NSForegroundColorAttributeName value:color range:range];
+        }
+        
+    }
+    
+    return attributedStr;
+    
+}
+
+
+
+
+#pragma mark - 6 Extract Method
+/**
+ *  获取某个字符串中子字符串的位置数组
+ *
+ *  @param totalString 总的字符串
+ *  @param subString   子字符串
+ *
+ *  @return 位置数组
+ */
++ (NSMutableArray *)sk_getRangeWithTotalString:(NSString *)totalString SubString:(NSString *)subString {
+    
+    NSMutableArray *arrayRanges = [NSMutableArray array];
+    
+    if (subString == nil && [subString isEqualToString:@""]) {
+        return nil;
+    }
+    
+    NSRange rang = [totalString rangeOfString:subString];
+    
+    if (rang.location != NSNotFound && rang.length != 0) {
+        
+        [arrayRanges addObject:[NSNumber valueWithRange:rang]];
+        
+        NSRange      rang1 = {0,0};
+        NSInteger location = 0;
+        NSInteger   length = 0;
+        
+        for (int i = 0;; i++) {
+            
+            if (0 == i) {
+                
+                location = rang.location + rang.length;
+                length = totalString.length - rang.location - rang.length;
+                rang1 = NSMakeRange(location, length);
+            } else {
+                
+                location = rang1.location + rang1.length;
+                length = totalString.length - rang1.location - rang1.length;
+                rang1 = NSMakeRange(location, length);
+            }
+            
+            rang1 = [totalString rangeOfString:subString options:NSCaseInsensitiveSearch range:rang1];
+            
+            if (rang1.location == NSNotFound && rang1.length == 0) {
+                
+                break;
+            } else {
+                
+                [arrayRanges addObject:[NSNumber valueWithRange:rang1]];
+            }
+        }
+        
+        return arrayRanges;
+    }
+    
+    return nil;
+}
+
 
 @end
